ef2f9d8fa818432ef590845bab0ceedc4a7ceac6
Sophie-WZR <164601603+Sophie-WZR@users.noreply.github.com>
2025-01-23 17:26:01 -0800

    step 3

--- global.js
+++ global.js
@@ -26,22 +26,48 @@ let pages = [
     { url: 'projects/', title: 'Projects' },
     { url: 'contact/', title: 'Contact' },
     { url: 'resume/', title: 'Resume' },
-    { url: 'https://github.com/Sophie-WZR', title: 'GitHub', external: true }
+    { url: 'https://github.com/Sophie-WZR', title: 'GitHub' }
   ];
   
   // Create the <nav> element
   let nav = document.createElement('nav');
   document.body.prepend(nav);
+
+  const ARE_WE_HOME = document.documentElement.classList.contains('home');
   
   // Dynamically add links to the navigation
   for (let p of pages) {
     let url = p.url;
     let title = p.title;
+    // TODO create link and add it to nav
+
+
+    console.log('adding to nav');
     if (url.startsWith('https://github.com')) {
-        nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
-        continue;
+        // github page
+        url = url
+    } else if (!ARE_WE_HOME && !url.startsWith('http')) {
+        // non home page
+        url = '../' + url;
+    } else {
+        // github page
+        url = 'https://sophie-wzr.github.io/portfolio/' + url
     }
-    url = 'https://sophie-wzr.github.io/portfolio/' + url;
-    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+
+    // home page
+    
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+
+    if (a.host === location.host && a.pathname === location.pathname) {
+        a.classList.add('current');
+    }
+
+    if (a.host !== location.host){
+        a.target = '_blank';
+    }
+
+    nav.append(a);
 }
-  
+
