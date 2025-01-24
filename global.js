5e5d8cb03d75b2863485c71da11992610d4c7656
Sophie-WZR <164601603+Sophie-WZR@users.noreply.github.com>
2025-01-23 18:13:17 -0800

    step 4

--- global.js
+++ global.js
@@ -112,3 +112,8 @@ form?.addEventListener("submit", (event) => {
   location.href = url;
 });
 
+
+select.addEventListener('input', function (event) {
+    console.log('color scheme changed to', event.target.value);
+    document.documentElement.style.setProperty('color-scheme', event.target.value);
+  });
