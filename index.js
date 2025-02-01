2ae068d5be472e7337bd8273a7d9c4783f214418
Sophie-WZR <164601603+Sophie-WZR@users.noreply.github.com>
2025-01-31 22:06:11 -0800

    update

--- index.js
+++ index.js
@@ -1,43 +1,23 @@
-// Import functions from global.js
-import { fetchJSON, renderProjects, fetchGitHubData } from './global.js';
+import { fetchJSON, renderProjects, fetchGitHubData} from './global.js';
 
-async function displayLatestProjects() {
-    try {
-        const projects = await fetchJSON('./lib/projects.json');
-        const latestProjects = projects.slice(0, 3);
+const projects = await fetchJSON('./lib/projects.json');
+const latestProjects = projects.slice(0, 3);
+console.log(latestProjects);
+const projectsContainer = document.querySelector('.projects');
 
-        const projectsContainer = document.querySelector('.projects');
-        if (projectsContainer) {
-            renderProjects(latestProjects, projectsContainer, 'h2');
-        } else {
-            console.error('Projects container not found on the homepage.');
-        }
-    } catch (error) {
-        console.error('Error loading or rendering projects:', error);
-    }
-}
+renderProjects(latestProjects, projectsContainer, 'h2');
 
-async function displayGitHubProfileStats() {
-    try {
-        const githubData = await fetchGitHubData('giorgianicolaou');
-        const profileStats = document.querySelector('#profile-stats');
+const githubData = await fetchGitHubData('JaydenHuang8');
 
-        if (profileStats && githubData) {
-            profileStats.innerHTML = `
-                <dl>
-                    <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
-                    <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
-                    <dt>Followers:</dt><dd>${githubData.followers}</dd>
-                    <dt>Following:</dt><dd>${githubData.following}</dd>
-                </dl>
-            `;
-        } else {
-            console.error('Profile stats container or GitHub data not found.');
-        }
-    } catch (error) {
-        console.error('Error fetching GitHub profile stats:', error);
-    }
-}
+const profileStats = document.querySelector('#profile-stats');
 
-displayLatestProjects();
-displayGitHubProfileStats();
+if (profileStats) {
+    profileStats.innerHTML = `
+          <dl>
+            <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
+            <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
+            <dt>Followers:</dt><dd>${githubData.followers}</dd>
+            <dt>Following:</dt><dd>${githubData.following}</dd>
+          </dl>
+      `;
+  }
