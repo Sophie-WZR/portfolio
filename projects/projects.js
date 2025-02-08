b65ebd89a6342def483f31f6d6a0a800987a0451
Sophie-WZR <164601603+Sophie-WZR@users.noreply.github.com>
2025-02-08 15:17:21 -0800

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -1,4 +1,5 @@
 import { fetchJSON, renderProjects, countProjects } from '../global.js';
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
 
 const projects = await fetchJSON('../lib/projects.json');
 
